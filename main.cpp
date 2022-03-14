256e73b6dd8ae650cd348abe0a5a35a949ab923e
ALDAWOUD-NADER <100731947+ALDAWOUD-NADER@users.noreply.github.com>
2022-03-14 17:47:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,23 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
-
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
+int a,b,c;
+    float areaR,areaT1,areaS,areaT2;
+    cout << "Inserire il valore di a"<<endl;
+    cout << "Inserire il valore di b"<<endl;
+    cout << "Inserire il valore di c"<<endl;
+    cin >> a >> b >> c;
+    areaR = (a*b);
+    areaT1= (a*b)/2;
+    areaS=(a*a);
+    areaT2=(0.5*(a+b)*c);
+    cout << "il valore di a è "<< a <<endl ;
+    cout << "il valore di b è "<< b <<endl;
+    cout << "il valore di c è "<< c <<endl;
+    cout << "area di rettangolo"<< areaR<<endl ;
+    cout << "area di triangolo"<< areaT1 <<endl;
+    cout << "area di quadrato"<< areaS <<endl;
+    cout << "are di trapezio "<< areaT2<<endl ;
 
 
 }
