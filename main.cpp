378c7221d255e3f68b802334e4c9ae5e30beea3c
ALDAWOUD-NADER <100731947+ALDAWOUD-NADER@users.noreply.github.com>
2022-03-14 22:38:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 using namespace std;
+int main() {
+    float a,b,c;
+    float Triangolo,Quadrato,Rettangolo,Trapezio;
 
-int main(){
- float a=0,b=0,c=0;
-    float areaR,areaT1,areaQ,areaT2;
-    cin >> a >> b >> c;
+    cin >> a >> b >> c ;
 
-    areaT1= (a*b)/2;
-    areaQ=a*a;
-    areaR=a*b;
-    areaT2=0.5*(a+b)*c;
+    Triangolo = (float) 0.5*(a*b);
+    Quadrato = a*a;
+    Rettangolo = a*b;
+    Trapezio= (float)0.5*(a*b)*c;
 
+    cout << Triangolo << endl;
+    cout << Quadrato << endl;
+    cout << Rettangolo << endl;
+    cout << Trapezio << endl;
 
-    cout << "area di triangolo"<< areaT1<<endl ;
-    cout << "area di quadrato"<< areaQ <<endl;
-    cout << "area di rettangolo"<< areaR<<endl;
-    cout << "area di trapezio "<< areaT2<<endl ;
- 
-return0;
-
-
-
+    return 0;
 }
